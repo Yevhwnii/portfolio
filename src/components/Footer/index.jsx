f9ceda8db4199f010a9ec39d9cce291b6d487cf3
Yevhwnii <mirandoos3223@gmail.com>
2021-01-31 19:39:27 +0100

    footer component is done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import './Footer.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='footer_leftSide'>
+        <Typography className='footer_name'>Yevhenii Moiseienko</Typography>
+      </div>
+      <div className='footer_rightSide'>
+        <Typography className='footer_copyright'>
+          All rights reserved © {new Date().getFullYear()}
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

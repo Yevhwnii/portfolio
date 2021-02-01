11f8ce466bae1db44a6fde0e91a9a948fdc1e61e
Yevhwnii <mirandoos3223@gmail.com>
2021-02-01 19:06:51 +0100

    About me page

---
+++ src/pages/AboutMe/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './AboutMe.css';
+import { Grid, Typography } from '@material-ui/core';
+
+import resume, { myServices, mySkills } from '../../utils/resumeData';
+import Card from '../../components/Card';
+import SkillGraph from '../../components/SkillGraph';
+
+const AboutMe = () => {
+  return (
+    <>
+      {/* About me section */}
+      <Grid container className='section' style={{ paddingBottom: 45 }}>
+        <Grid item className='section_title' style={{ marginBottom: 30 }}>
+          <span></span>
+          <h6>About Me</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography className='about_me_text' variant='body2'>
+            {resume.aboutMe}
+          </Typography>
+        </Grid>
+      </Grid>
+      {/* Personal Characteristics */}
+      <Grid container className='section' style={{ paddingBottom: 45 }}>
+        <Grid item className='section_title' style={{ marginBottom: 30 }}>
+          <span></span>
+          <h6>Personal Characteristics</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container spacing={3} justify='space-around'>
+            {myServices.map((char) => {
+              return (
+                <Card
+                  icon={char.icon}
+                  title={char.title}
+                  desc={char.description}
+                />
+              );
+            })}
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* My skills */}
+      <Grid
+        container
+        justify='space-between'
+        spacing={3}
+        className='section'
+        style={{ paddingBottom: 45 }}>
+        {mySkills.map((skill) => {
+          return (
+            <Grid item xs={12} sm={6}>
+              <SkillGraph title={skill.title} tech={skill.technologies} />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Grid container className=''></Grid>
+      <Grid container className=''></Grid>
+    </>
+  );
+};
+
+export default AboutMe;

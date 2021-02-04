22b1e0a955fa051ebc13375ff0b66d25b952c7e5
Yevhwnii <mirandoos3223@gmail.com>
2021-02-04 19:25:16 +0100

    warning fix

--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -10,79 +10,92 @@ import CustomTimeline, {
   CustomTimelineSeparator,
 } from '../../components/Timeline';
 import { workTimeline, educationTimeline } from '../../utils/resumeData';
+import FadeDiv from '../../components/Fade';
 
 const Resume = () => {
   return (
-    <Grid container className='section' style={{ paddingBottom: 45 }}>
-      <Grid item className='section_title' style={{ marginBottom: 30 }}>
-        <span></span>
-        <h6>Resume</h6>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid item container>
-          <Grid item sm={12} md={6}>
-            {/* Working History timeline */}
-            <CustomTimeline title='Working history' icon={<WorkIcon />}>
-              {workTimeline.map((work, i) => {
-                return (
-                  <TimelineItem>
-                    <CustomTimelineSeparator />
-                    <TimelineContent
-                      style={
-                        i === workTimeline.length - 1 ? { marginBottom: 3 } : {}
-                      }
-                      className='timeline_content'>
-                      <Typography className='timeline_title'>
-                        {work.title}
-                      </Typography>
-                      <Typography variant='caption' className='timeline_date'>
-                        {work.date}
-                      </Typography>
-                      <Typography
-                        variant='body2'
-                        className='timeline_description'>
-                        {work.description}
-                      </Typography>
-                    </TimelineContent>
-                  </TimelineItem>
-                );
-              })}
-            </CustomTimeline>
+    <>
+      <FadeDiv>
+        <Grid container className='section' style={{ paddingBottom: 45 }}>
+          <Grid item className='section_title' style={{ marginBottom: 30 }}>
+            <span></span>
+            <h6>Experience</h6>
           </Grid>
-          <Grid item sm={12} md={6}>
-            {/* Education History timeline */}
-            <CustomTimeline title='Education history' icon={<EducationIcon />}>
-              {educationTimeline.map((education, i) => {
-                return (
-                  <TimelineItem>
-                    <CustomTimelineSeparator />
-                    <TimelineContent
-                      style={
-                        i === educationTimeline.length - 1
-                          ? { marginBottom: 3 }
-                          : {}
-                      }
-                      className='timeline_content'>
-                      <Typography className='timeline_title'>
-                        {education.title}
-                      </Typography>
-                      <Typography variant='caption' className='timeline_date'>
-                        {education.date}
-                      </Typography>
-                      <Typography
-                        variant='body2'
-                        className='timeline_description'>
-                        {education.description}
-                      </Typography>
-                    </TimelineContent>
-                  </TimelineItem>
-                );
-              })}
-            </CustomTimeline>
+          <Grid item xs={12}>
+            <Grid item container>
+              <Grid item sm={12} md={6}>
+                {/* Working History timeline */}
+                <CustomTimeline title='Working history' icon={<WorkIcon />}>
+                  {workTimeline.map((work, i) => {
+                    return (
+                      <TimelineItem key={work.title}>
+                        <CustomTimelineSeparator />
+                        <TimelineContent
+                          style={
+                            i === workTimeline.length - 1
+                              ? { marginBottom: 3 }
+                              : {}
+                          }
+                          className='timeline_content'>
+                          <Typography className='timeline_title'>
+                            {work.title}
+                          </Typography>
+                          <Typography
+                            variant='caption'
+                            className='timeline_date'>
+                            {work.date}
+                          </Typography>
+                          <Typography
+                            variant='body2'
+                            className='timeline_description'>
+                            {work.description}
+                          </Typography>
+                        </TimelineContent>
+                      </TimelineItem>
+                    );
+                  })}
+                </CustomTimeline>
+              </Grid>
+              <Grid item sm={12} md={6}>
+                {/* Education History timeline */}
+                <CustomTimeline
+                  title='Education history'
+                  icon={<EducationIcon />}>
+                  {educationTimeline.map((education, i) => {
+                    return (
+                      <TimelineItem key={education.title}>
+                        <CustomTimelineSeparator />
+                        <TimelineContent
+                          style={
+                            i === educationTimeline.length - 1
+                              ? { marginBottom: 3 }
+                              : {}
+                          }
+                          className='timeline_content'>
+                          <Typography className='timeline_title'>
+                            {education.title}
+                          </Typography>
+                          <Typography
+                            variant='caption'
+                            className='timeline_date'>
+                            {education.date}
+                          </Typography>
+                          <Typography
+                            variant='body2'
+                            className='timeline_description'>
+                            {education.description}
+                          </Typography>
+                        </TimelineContent>
+                      </TimelineItem>
+                    );
+                  })}
+                </CustomTimeline>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
+      </FadeDiv>
+    </>
   );
 };
 
